3c132486569c49cee0b790da33e7f33dccdf3819
GBRodrgues <gabrielcr18@outlook.com>
2025-11-18 19:44:29 -0300

    Use wouter Router base for GitHub Pages route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Switch, Router as WouterRouter } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
@@ -12,16 +12,18 @@ import Prototype4 from "./pages/Prototype4";
 
 function Router() {
   return (
-    <Switch>
-      <Route path={"/ablaze-prototypes"} component={Home} />
-      <Route path={"/ablaze-prototypes/prototype1"} component={Prototype1} />
-      <Route path={"/ablaze-prototypes/prototype2"} component={Prototype2} />
-      <Route path={"/ablaze-prototypes/prototype3"} component={Prototype3} />
-      <Route path={"/ablaze-prototypes/prototype4"} component={Prototype4} />
-      <Route path={"/ablaze-prototypes/404"} component={NotFound} />
-      {/* Final fallback route */}
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base="/ablaze-prototypes">
+      <Switch>
+        <Route path={"/"} component={Home} />
+        <Route path={"/prototype1"} component={Prototype1} />
+        <Route path={"/prototype2"} component={Prototype2} />
+        <Route path={"/prototype3"} component={Prototype3} />
+        <Route path={"/prototype4"} component={Prototype4} />
+        <Route path={"/404"} component={NotFound} />
+        {/* Final fallback route */}
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
